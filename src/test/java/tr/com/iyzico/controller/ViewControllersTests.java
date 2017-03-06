4f1b5e82a9f39e5a4ad36dcce10ca7435b463f96
Murat Bayram <m.byram@gmail.com>
2017-03-06 13:34:45 +0300

    controller test profile setting

--- src/test/java/tr/com/iyzico/controller/ViewControllersTests.java
+++ src/test/java/tr/com/iyzico/controller/ViewControllersTests.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("local")
 public class ViewControllersTests {
 
 	@Autowired
