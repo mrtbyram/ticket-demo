2569e3d4a4506b65721ca4b1961c4765809659aa
Murat Bayram <m.byram@gmail.com>
2017-04-17 00:48:34 +0300

    remove unnecessary config import

--- src/main/java/tr/com/demo/TicketApplication.java
+++ src/main/java/tr/com/demo/TicketApplication.java
@@ -2,12 +2,10 @@ package tr.com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@Import(DatabaseConfig.class)
 public class TicketApplication {
 
 	public static void main(String[] args) {
